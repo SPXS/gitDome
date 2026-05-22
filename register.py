1da632f36b984891f7aaacd6a59d56f1d8354dcd
张科锋 <kefeng.zhang@ecidh.com>
2026-05-22 13:56:51 +0800

    添加 commite

--- register.py
+++ register.py
@@ -1,3 +1,4 @@
+
 def register(username, email, password):
     # main 分支：添加密码强度检查
     if len(password) < 6:
@@ -6,5 +7,5 @@ def register(username, email, password):
     return True
 def send_verification_email(email):
     pass
-// 我是 gitDome 的注册模块，负责处理用户注册和发送验证邮件的功能。
-// 我是抢地盘的, 我是胸大的, 我是腿长的, 我是屁股大的
+
+
